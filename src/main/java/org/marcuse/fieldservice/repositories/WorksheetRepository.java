ea8607e199a348a32eba77df0cd71be264d5a872
Beltsazar <beltsazar.register@gmail.com>
2016-04-19 22:50:11 +0200

    problems fixed!

--- src/main/java/org/marcuse/fieldservice/repositories/WorksheetRepository.java
+++ src/main/java/org/marcuse/fieldservice/repositories/WorksheetRepository.java
@@ -1,7 +1,17 @@
 package org.marcuse.fieldservice.repositories;
 
-/**
- * Created by danielmarcuse on 19/04/16.
- */
-public class WorksheetRepository {
+import org.hibernate.metamodel.source.binder.Sortable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RepositoryRestResource
+public interface WorksheetRepository extends PagingAndSortingRepository<Worksheet, Long> {
+
 }

87d5183c03f93191f05de7aa799ef28ea9f65e86
Beltsazar <beltsazar.register@gmail.com>
2016-04-22 12:57:51 +0200

    basic auth working

---
+++ src/main/java/org/marcuse/fieldservice/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package org.marcuse.fieldservice.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				.antMatchers("/**").fullyAuthenticated()
+				.and().httpBasic();
+	}
+
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//
+//		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+//		auth.inMemoryAuthentication().withUser("ravan").password("ravan123").roles("USER");
+//		auth.inMemoryAuthentication().withUser("kans").password("kans123").roles("USER");
+//	}
+
+
+}

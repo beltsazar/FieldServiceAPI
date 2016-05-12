52a6db2daab5954abcd9d8d7de361ec33bf0e910
Beltsazar <beltsazar.register@gmail.com>
2016-05-12 21:39:46 +0200

    minor fix

--- src/main/java/org/marcuse/fieldservice/repositories/WorksheetRepository.java
+++ src/main/java/org/marcuse/fieldservice/repositories/WorksheetRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 @RepositoryRestResource
 public interface WorksheetRepository extends PagingAndSortingRepository<Worksheet, Long> {
+	@Transactional
 	List<Worksheet> findByVisible(@Param("visible") boolean visible);
 }

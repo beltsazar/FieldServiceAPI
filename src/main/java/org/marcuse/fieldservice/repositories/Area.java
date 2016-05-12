189a722a2f87e2e6d47d31fb5b5e714151493c49
Beltsazar <beltsazar.register@gmail.com>
2016-05-12 20:37:49 +0200

    Map field for Area

--- src/main/java/org/marcuse/fieldservice/repositories/Area.java
+++ src/main/java/org/marcuse/fieldservice/repositories/Area.java
@@ -1,9 +1,17 @@
 package org.marcuse.fieldservice.repositories;
 
+import com.fasterxml.jackson.annotation.JsonRawValue;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.IOException;
 import java.util.List;
 
 @Entity
@@ -18,6 +26,13 @@ public class Area {
 	@Setter
 	private int number;
 
+	@Lob
+	@JsonRawValue
+	@JsonDeserialize(using = ShapeDeserializer.class)
+	@Getter
+	@Setter
+	private String shape;
+
 	@Getter
 	@ManyToOne
 	private City city;
@@ -28,5 +43,16 @@ public class Area {
 	@OneToMany(mappedBy = "area")
 	private List<Assignment> assignments;
 
+}
+
+class ShapeDeserializer extends JsonDeserializer<String>
+{
+	@Override
+	public String deserialize(JsonParser jsonparser,
+							  DeserializationContext deserializationcontext) throws IOException, JsonProcessingException {
+
+		JsonNode node = jsonparser.getCodec().readTree(jsonparser);
+		return node.toString();
+	}
 
 }
--- src/main/java/org/marcuse/fieldservice/repositories/AreaProjectionEntities.java
+++ src/main/java/org/marcuse/fieldservice/repositories/AreaProjectionEntities.java
@@ -1,13 +1,17 @@
 package org.marcuse.fieldservice.repositories;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.data.rest.core.config.Projection;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Projection(name = "entities", types = Area.class)
 public interface AreaProjectionEntities {
 
 	long getId();
 
 	int getNumber();
+
+	String getShape();
 
 	City getCity();
 

c49bccb5b4a7f615a24a365dee85e4176960c0aa
Beltsazar <beltsazar.register@gmail.com>
2016-06-02 19:55:45 +0200

    assignment find by campaign

--- src/main/java/org/marcuse/fieldservice/repositories/AssignmentRepository.java
+++ src/main/java/org/marcuse/fieldservice/repositories/AssignmentRepository.java
@@ -2,10 +2,12 @@ package org.marcuse.fieldservice.repositories;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,5 +16,7 @@ import java.util.List;
 public interface AssignmentRepository extends PagingAndSortingRepository<Assignment, Long> {
 
 	List<Assignment> findByArea(@Param("area") Area area);
+	@Transactional
+	Page<Assignment> findByCampaign(@Param("campaign") Campaign campaign, Pageable page);
 
 }
